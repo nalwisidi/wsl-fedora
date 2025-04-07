77d10cd51c95d7beab546c1a446ff81f893f1fae
Nawaf Alwisidi <31031980+nalwisidi@users.noreply.github.com>
2025-04-07 07:22:19 +0300

    UPDATE: fixed a duplicate code in create_user.sh

--- scripts/create_user.sh
+++ scripts/create_user.sh
@@ -9,26 +9,7 @@ passwd "$username"
 usermod -aG wheel "$username"
 echo "$username" > /username_created
 
-cat <<'EOF' > "/home/$username/.initial_setup.sh"
-#!/bin/bash
-echo "[bootstrap] running initial setup..."
-curl -fsSL https://raw.githubusercontent.com/nalwisidi/wsl-fedora/main/scripts/bootstrap.sh | bash
-EOF
-
-chmod +x "/home/$username/.initial_setup.sh"
-chown "$username:$username" "/home/$username/.initial_setup.sh"
-#!/bin/bash
-
-echo -n "Enter new UNIX username: "
-read username
-
-useradd -m -s /bin/zsh "$username"
-echo "Set password for $username:"
-passwd "$username"
-usermod -aG wheel "$username"
-echo "$username" > /username_created
-
-cat <<'EOF' > "/home/$username/.initial_setup.sh"
+cat <<EOF > "/home/$username/.initial_setup.sh"
 #!/bin/bash
 echo "[bootstrap] running initial setup..."
 curl -fsSL https://raw.githubusercontent.com/nalwisidi/wsl-fedora/main/scripts/bootstrap.sh | bash
